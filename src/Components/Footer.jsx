5534c7a9ef35e7330f6d0c3b7a8f6770f7b07db3
LuisaVAZ <lilo_va@hotmail.com>
2022-12-14 13:02:09 +0100

    Header and footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { FcCustomerSupport } from "react-icons/fa";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <p> FemCoders Gaby&Luisa <FcCustomerSupport /> 2022</p>
+    )
+  }
+}
+
+export default Footer
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <p className='txtHeader'> Sakura card captor Tarot </p>
+    )
+  }
+}
+
+export default Header
