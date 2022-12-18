0a973c45839cdf801c9d9e667685821db1bb6f42
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2022-12-18 23:32:55 +0100

    card de tailwinf iniciando

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [
+    // ...
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // ...
+  ],
 }
--- src/Pages/Reading.jsx
+++ src/Pages/Reading.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Card from '../components/Card'
 
 function Reading() {
   return (
     <>
         <h1>Reading Page</h1>
+        <Card/>
         <Link to="/Result" >Ir a Resultado de Lectura</Link>
     </>
   )
