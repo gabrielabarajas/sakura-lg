84fefc4eb1c9059232f813ae91cc3ab99484eeff
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2022-12-18 18:58:57 +0100

    diseño de la card

---
+++ src/Components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <>
+    <div className="card">
+        <img src="..." className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <p className="card-text">Card name</p>
+        </div>
+    </div>
+</>
+)
+}
+
+export default Card
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import { FcCustomerSupport } from "react-icons/fa";
 
 export class Footer extends Component {
   render() {
     return (
-      <p> FemCoders Gaby&Luisa <FcCustomerSupport /> 2022</p>
+      <p> FemCoders Gaby&Luisa 2022</p>
     )
   }
 }
