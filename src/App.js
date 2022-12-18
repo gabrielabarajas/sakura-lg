fef7569b75cbb2dd02beeed8e9a53287b933524f
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2022-12-18 21:25:12 +0100

    preparacion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import Header from './Components/Header';
 import './App.css';
-import Footer from './Components/Footer';
+import Card from './components/Card';
 
 function App() {
   return (
     <>
-      <Header/>
-      <Footer/>
-
+      <Card/>
     </>
   );
 }
