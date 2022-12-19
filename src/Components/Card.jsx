903db55f33f17a253bf71f76d09b895fe583b73f
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2022-12-19 18:44:52 +0100

    card using tailwinds again

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,9 +4,11 @@ import "../styles/card.css"
 function Card({key, image, name, title}) {
   return (
     <>
-        <div>
-            <img className = "aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8" key = {key} src ={image} alt ={name}/>
-            <p className="mt-1 text-lg font-medium text-gray-900">{title}</p>
+        <div className="group" id="card-color">
+            <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-3xl bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                <img key = {key} src ={image} alt ={name} className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl"/>
+            </div>
+            <h3 className="mt-4 text-sm text-gray-700">{title}</h3>
         </div>
     </>
 )
--- src/Components/CardPanel.jsx
+++ src/Components/CardPanel.jsx
@@ -8,11 +8,17 @@ function CardPanel() {
 
   return (
     <>
-        <div className="cont-cards">
-            {cards!==[] ? cards.map((item,index)=>(
-              <Card key = {index} image = {item.sakuraCard} name = {item.spanishName} title = {item.spanishName}/>
-            )):"No hay cards"}
-          </div>
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <h2 className="sr-only">Products</h2>
+            <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                {cards!==[] ? cards.map((item,index)=>(
+                  <Card key = {index} image = {item.sakuraCard} name = {item.spanishName} title = {item.spanishName}/>
+                )):"No hay cards"} 
+            </div>
+        </div>
+      </div>
+
     </>
   )
 }
