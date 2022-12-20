54f12344481c5382e389fe37e92669478e7ed8ef
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2022-12-20 19:24:01 +0100

    funcion para llenar array

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -14,6 +14,9 @@ const handleClick = () =>{
 }
 
 const handleAddCard = (key, image, name, title) => {
+    console.log(key);
+    console.log(image);
+    console.log(name);
     const newCard = {
         key : {key},
         image : {image},
@@ -23,14 +26,14 @@ const handleAddCard = (key, image, name, title) => {
     setReadCards([...readCards,newCard]);
     console.log(readCards);
 }
-  
+
 return (
     <>
         <div className="group" id="card-color">
             <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-3xl bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                 <ReactCardFlip isFlipped={isFlipped}>
-                    <img src ={backFace} alt ='back face card' className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl" onClick = { () => {handleClick(); handleAddCard();}}/>
-                    <img key = {key} src = {image} alt ={name} className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl" onClick={handleClick}/>
+                    <img src ={backFace} alt ='back face card' className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl" onClick = { () => {handleClick(); handleAddCard(Card);}}/>
+                    <img key = {key} src = {image} alt ={name} className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl"/>
                 </ReactCardFlip>
             </div>
         </div>
