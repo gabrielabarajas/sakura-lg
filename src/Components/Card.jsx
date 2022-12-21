9fdef029ce64cab020e75102798622cb536077e0
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2022-12-21 21:40:45 +0100

    changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -11,7 +11,6 @@ const [readCards, setReadCards] = useState([]);
 
 const handleClick = () =>{
     setIsFlipped(!isFlipped);
-
 }
 
 const handleAddCard = (card) => {
@@ -24,7 +23,6 @@ return (
     <>
         <div className="group" id="card-color">
             <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-3xl bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-
                 <ReactCardFlip isFlipped={isFlipped}>
                     <img src ={backFace} alt ='back face card' className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl" onClick = { () => {handleClick(); handleAddCard();}}/>
                     <img key = {key} src = {image} alt ={name} className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl"/>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "../styles/header.css"
+import "../styles/Header.css"
 import { Link } from 'react-router-dom'
 
 function Header() {
