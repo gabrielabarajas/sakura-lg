1b4fa2fb920db995a9e04634dd86218050aebacc
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2022-12-21 15:52:58 +0100

    changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,14 +3,13 @@ import "../styles/card.css";
 import ReactCardFlip from 'react-card-flip';
 import backFace from '../images/SakuraBackFace.jpg'
 
-function Card({key, image, name, title}) {
+function Card() {
 
 const [isFlipped, setIsFlipped] = useState(false);
 
 const [readCards, setReadCards] = useState([]);
 
 const handleClick = () =>{
-    console.log();
     setIsFlipped(!isFlipped);
 
 }
@@ -25,9 +24,10 @@ return (
     <>
         <div className="group" id="card-color">
             <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-3xl bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+
                 <ReactCardFlip isFlipped={isFlipped}>
                     <img src ={backFace} alt ='back face card' className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl" onClick = { () => {handleClick(); handleAddCard();}}/>
-                    <img key = {key} src = {image} alt ={name} className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl"/>
+                    <img key = {this.key} src = {this.image} alt ={this.name} className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl"/>
                 </ReactCardFlip>
             </div>
         </div>
--- src/Components/CardPanel.jsx
+++ src/Components/CardPanel.jsx
@@ -11,10 +11,9 @@ function CardPanel() {
 
   return (
     <>
-      
-      <div className="bg-white"> 
+      <div className="bg-white">
         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            <h2 className="txt-select-msg">Selecciona tres cartas</h2>
+            <h2 className="sr-only">Products</h2>
             <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 {cards!==[] ? cards.map((item,index)=>(
                   <Card key = {index} image = {item.sakuraCard} name = {item.spanishName} title = {item.spanishName}/>
