41debe6e735cc1786075727fdba75d50e03c2915
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2022-12-22 11:52:52 +0100

    changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -25,10 +25,9 @@ return (
     <>
         <div className="group" id="card-color">
             <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-3xl bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-
                 <ReactCardFlip isFlipped={isFlipped}>
-                    <img src ={backFace} alt ='back face card' className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl" onClick = {handleClick}/>
-                    <img key={key} src = {image} alt ={name} className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl"/>
+                    <img src ={backFace} alt ='back face card' className="h-30 w-30 group-hover:opacity-75 rounded-3xl" onClick = {handleClick}/>
+                    <img key={key} src = {image} alt ={name} className="h-30 w-30 group-hover:opacity-75 rounded-3xl"/>
                 </ReactCardFlip>
             </div>
         </div>
